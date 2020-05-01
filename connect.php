f71c9c79c95aca46f2ac2a0789e0afabce3aba02
nancyaprelia24 <64649384+nancyaprelia24@users.noreply.github.com>
2020-05-01 23:32:28 +0700

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//connect to database
-	$connectdb = mysqli_connect("localhost","root","","bankbung");
-	//checking
-	if(mysqli_connect_errno()){
-		echo "Silahkan Cek Data Base Kamu";
-		exit;
-	}
-?>
