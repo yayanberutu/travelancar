382d17633241b14889b20cc12396e9fb90b6ffae
Nancy Aprelia Sibarani <nancysibarani204@gmail.com>
2020-05-10 21:36:57 +0700

    menambah ui bank konservatif

---
+++ App/App/UI/Bank Konservatif/Bank Konservatif/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bank_Konservatif.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Pilihan()
+        {
+            return View();
+        }
+
+        public ActionResult Transaksi()
+        {
+            return View();
+        }
+
+        public ActionResult Transfer()
+        {
+            return View();
+        }
+
+        public ActionResult Finish()
+        {
+            return View();
+        }
+
+    }
+}
