98bfb71751c8036018b70db9d607c9a434512976
Yosepri Disyandro Berutu <=>
2020-05-30 01:24:10 +0700

    menambahi BDO pembayaran pada bank konservatif

---
+++ App/App/UI/Bank Konservatif/BusinessDomainObject/PembayaranBDO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessDomainObject
+{
+    public class PembayaranBDO
+    {
+        public string invoice_number { get; set; }
+        public DateTime waktu_pesan { get; set; }
+        public DateTime waktu_bayar { get; set; }
+        public double nominal { get; set; }
+        public int status_bayar { get; set; }
+    }
+}
