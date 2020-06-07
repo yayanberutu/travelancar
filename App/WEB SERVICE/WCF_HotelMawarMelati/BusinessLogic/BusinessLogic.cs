ed43b0b82d52e4a6df553b8999f82917236cbb1c
Yosepri Disyandro Berutu <=>
2020-06-07 17:26:17 +0700

    menambah service pada hotelMawarMelati

---
+++ App/WEB SERVICE/WCF_HotelMawarMelati/BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    
+    public class KamarBL
+    {
+        BusinessDomainObject.KamarBDO kamarBDO = new BusinessDomainObject.KamarBDO();
+        public IEnumerable<BusinessDomainObject.KamarBDO> getAllKamar()
+        {
+            return kamarBDO.getAllKamar();
+        }
+    }
+
+    public class BookingBL
+    {
+        BusinessDomainObject.BookingBDO bookingBDO = new BusinessDomainObject.BookingBDO();
+        public void InsertBooking(BusinessDomainObject.BookingBDO booking,  BusinessDomainObject.KamarBDO kmr)
+        {
+            bookingBDO.InsertBooking(booking, kmr);
+        }
+
+    }
+}
